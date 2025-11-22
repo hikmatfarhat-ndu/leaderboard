16221af3002190dbfa7eeb89cba72185d045aba9
Hikmat Farhat <h.farhat@soton.ac.uk>
2025-11-22 10:02:26 +0000

    implemented clear_score

--- leaderboard.py
+++ leaderboard.py
@@ -9,3 +9,8 @@ def add_player(leaderboard:dict[str,timedelta],player_name:str)->bool:
         return False
     leaderboard.update({player_name:None})
     return True
+def clear_score(leaderboard,player_name):
+    if player_name not in leaderboard:
+        return False
+    leaderboard.update({player_name:None})
+    return True
