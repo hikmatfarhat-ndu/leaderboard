66d6ba1f77500f23cab4a5f5032762111df594a0
Hikmat Farhat <h.farhat@soton.ac.uk>
2025-11-22 09:59:03 +0000

    implemented add_run

--- leaderboard.py
+++ leaderboard.py
@@ -9,3 +9,13 @@ def add_player(leaderboard:dict[str,timedelta],player_name:str)->bool:
         return False
     leaderboard.update({player_name:None})
     return True
+
+def add_run(leaderboard:dict[str,timedelta],player_name:str,time:timedelta)->int:
+    if time.total_seconds()<0:
+        return 1
+    if player_name not in leaderboard:
+        return 2
+    
+    if leaderboard[player_name]==None or leaderboard[player_name]> time:
+        leaderboard.update({player_name:time})
+    return 0
